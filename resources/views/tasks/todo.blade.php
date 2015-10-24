a6f1d13a9dbe585fda894b63ffa8e3d982ba1b83
Will <wjones1977@gmail.com>
2015-10-24 07:40:53 -0400

    Add the ability to mark tasks complete, disable the csrf class in app/Http/Kernel.php temporarily

--- resources/views/tasks/todo.blade.php
+++ resources/views/tasks/todo.blade.php
@@ -6,8 +6,8 @@
         
         <style type="text/css">
         	th{
-                padding-left: 3px;
-                padding-right: 3px;
+                padding: 6px;
+                /*padding-right: 3px;*/
             }
             td{
         		border: 1px solid #ccc;
@@ -21,18 +21,41 @@
         	<table>
         		<th>From</th>
         		<th>Description</th>
-        		<th>Due Date/Time</th>
+                <th>Due Date/Time</th>
 	        	<?php 	foreach($tasks as $task):  ?> 
         					<tr>
 	        					<td><?= $task['task_creater']; ?></td>
         						<td><?= $task['description']; ?></td>
         						<td><?= $task['due_date_time']; ?></td>
-        						<td></td>
+        						<!-- Complete Task Button -->
+                                    <td>
+                                        <div    class="completeTaskButton" 
+                                                title="Mark Task Complete"
+                                                data-taskid="<?= $task['id']; ?>"
+                                                style=""
+                                                >
+                                            <span class="glyphicon glyphicon-remove" style="color: #a94442;"></span>
+                                        </div>
+                                    </td>
         					</tr>
-	        				
 	        	<?php 	endforeach; ?> 
         	</table>
         <pre style="margin-top: 10px">tasks/todo.blade.php</pre>
     </div>
+<!-- For Scripts to work.. they need to be inside of the blade template -->
+    <script type="text/javascript">
+        $(".completeTaskButton").on('click', function(){
+            var taskid = $(this).attr('data-taskid');
+            $.ajax({
+                type:   'POST',
+                url:    'markTaskComplete',
+                data: { taskid: taskid },
+                success: function(taskid){
+                    window.location.reload();
+                }
+            });
+        });
+    </script>
 @stop
+
 <?php error_log("todo view loaded successfully"); ?>
