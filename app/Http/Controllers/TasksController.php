a7d864b9562702021f11862669b0d2fb1649e69d
Will <wjones1977@gmail.com>
2015-10-25 04:07:28 -0400

    Add the task created date/time when adding a task

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -1,28 +1,21 @@
-<?php	namespace App\Http\Controllers;
+<?php	
+// TasksController.php
 
+namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Tasks\Tasks;
-// TasksController.php
 
 class TasksController extends Controller{
 	public function showAllUncompletedTasksAssignedToUser(){
 		$tasks = Tasks::where('completed_date_time',  '0000-00-00 00:00:00')
 					  ->where('task_recipient', \Auth::user()->name )->paginate(5);
 		return view('tasks.todo')->with('tasks', $tasks);		
-	
-
-
-
-
-
-
-
-
 	}
+	// Show the addTask view
 	public function showAddTask(){
 		return view('tasks.addTask');		
-
 	}
+	// Add a task to the `tasks` table 
 	public function addTask(){
 		$postedTask = \Input::all();
 		$taskRecipient = \Input::get('taskRecipient');
@@ -31,6 +24,7 @@ class TasksController extends Controller{
 		$newTask = new Tasks;
 		$newTask->task_recipient = $taskRecipient;
 		$newTask->description    = $taskDescription;
+		$newTask->created_date_time = date('Y-m-d H:i:s');
 		$newTask->due_date_time  = $dueDate;
 		$newTask->task_creater   = $user = \Auth::user()->name;
 		$newTask->save();
@@ -38,7 +32,7 @@ class TasksController extends Controller{
 	}
 	public function markTaskComplete(){
 		$taskId = \Input::get('taskid');
-		echo $taskId;
+		//echo $taskId;
 		$taskToUpdate = Tasks::where('id', $taskId )->first();
 		$taskToUpdate->completed_date_time = date('Y-m-d H:i:s');
 		$taskToUpdate->save();
